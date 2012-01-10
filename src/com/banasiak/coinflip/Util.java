60c7276e31cf8c425a2dbd19255d4335217ce64e
unknown <alex@.(none)>
2012-01-10 15:56:54 -0600

    Updated the library loading code to load multiple external resource files, not just the one hardcoded file.
    Also made the animation duration a variable : )

--- src/com/banasiak/coinflip/Util.java
+++ src/com/banasiak/coinflip/Util.java
@@ -1,27 +1,26 @@
 /*
- *========================================================================
- * Util.java
- * Dec 30, 2011 12:46:05 PM | variable
- * Copyright (c) 2011 Richard Banasiak
- *========================================================================
- * This file is part of CoinFlip.
- *
- *    CoinFlip is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    CoinFlip is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with CoinFlip.  If not, see <http://www.gnu.org/licenses/>.
+ * ========================================================================
+ * Util.java Dec 30, 2011 12:46:05 PM | variable Copyright (c) 2011 Richard
+ * Banasiak
+ * ======================================================================== This
+ * file is part of CoinFlip.
+ * 
+ * CoinFlip is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * CoinFlip is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * CoinFlip. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.banasiak.coinflip;
 
+import java.util.List;
 import java.util.Random;
 
 import android.content.Context;
@@ -37,29 +36,30 @@ public class Util
 
     private final Context mContext;
 
-    public Util(Context context)
+    public Util(final Context context)
     {
         mContext = context;
     }
 
     // check to see if an extension package is installed
-    public boolean isExtPkgInstalled(String extPkg)
+    public boolean isExtPkgInstalled(final String extPkg)
     {
         Log.d(TAG, "isExtPkgInstalled()");
-        Log.d(TAG, "extPkg="+ extPkg);
+        Log.d(TAG, "extPkg=" + extPkg);
         boolean isInstalled = false;
         try
         {
-            PackageInfo packageInfo = mContext.getPackageManager().getPackageInfo(extPkg, 0);
+            final PackageInfo packageInfo = mContext.getPackageManager()
+                .getPackageInfo(extPkg, 0);
             if (packageInfo != null)
             {
                 isInstalled = true;
             }
         }
-        catch (NameNotFoundException e)
+        catch (final NameNotFoundException e)
         {
             Log.d(TAG, "NameNotFoundException");
-            //e.printStackTrace();
+            // e.printStackTrace();
         }
 
         Log.d(TAG, "result=" + isInstalled);
@@ -68,62 +68,130 @@ public class Util
     }
 
     // concatenate two arrays
-    public CharSequence[] mergeArray(CharSequence[] a, CharSequence[] b)
+    public CharSequence[] mergeArray(final CharSequence[] a,
+        final CharSequence[] b)
     {
         Log.d(TAG, "mergeArray()");
-        CharSequence[] result = new CharSequence[a.length + b.length];
+        final CharSequence[] result = new CharSequence[a.length + b.length];
         int i, j;
 
-        for(i=0; i<a.length; i++)
+        for (i = 0; i < a.length; i++)
         {
             result[i] = a[i];
         }
 
-        for(j=0; j<b.length; j++)
+        for (j = 0; j < b.length; j++)
         {
-            result[i+j] = b[j];
+            result[i + j] = b[j];
         }
 
         return result;
     }
 
     // build an array of all possible coins and select a random one
-    public String getRandomCoin(String extPkg)
+    public String getRandomCoin()
     {
         Log.d(TAG, "getRandomCoin()");
         String coin = "default";
         int val = -1;
-        Random generator = new Random();
-        if( isExtPkgInstalled(extPkg))
+        final Random generator = new Random();
+        final List<PackageInfo> pkgs = findExternalPackages();
+
+        // load the built-in values
+        CharSequence[] currentEntryValues = mContext
+            .getResources().getStringArray(R.array.coins_values);
+
+        for (final PackageInfo info : pkgs)
         {
             try
             {
-                // load the built-in values
-                CharSequence[] currentEntryValues = mContext.getResources().getStringArray(R.array.coins_values);
-
                 // load the resources from the add-in package
-                Resources extPkgResources = mContext.getPackageManager().getResourcesForApplication(extPkg);
+                final Resources extPkgResources = mContext.getPackageManager()
+                    .getResourcesForApplication(info.packageName);
 
                 // load the values in the add-in package
-                int coinsValuesId = extPkgResources.getIdentifier("coins_values", "array", extPkg);
-                CharSequence[] newEntryValues = extPkgResources.getStringArray(coinsValuesId);
+                final int coinsValuesId = extPkgResources.getIdentifier(
+                    "coins_values", "array", (info.packageName));
+                final CharSequence[] newEntryValues = extPkgResources
+                    .getStringArray(coinsValuesId);
 
                 // merge the two values
-                CharSequence[] combinedEntryValues = mergeArray(currentEntryValues, newEntryValues);
-
-                // choose a random item from the array
-                val = generator.nextInt(combinedEntryValues.length);
-                coin = (String) combinedEntryValues[val];
-
+                currentEntryValues = mergeArray(currentEntryValues, newEntryValues);
             }
-            catch (NameNotFoundException e)
+            catch (final NameNotFoundException e)
             {
-                // shouldn't happen because we already verified the package exists
+                // shouldn't happen because we already verified the package
+                // exists
                 Log.e(TAG, "NameNotFoundException", e);
             }
         }
+
+        // choose a random item from the array
+        val = generator.nextInt(currentEntryValues.length);
+        coin = (String) currentEntryValues[val];
+
         Log.d(TAG, "result=" + coin);
         return coin;
+    }
+
+    public List<PackageInfo> findExternalPackages()
+    {
+        final List<PackageInfo> packageInfo = mContext.getPackageManager().getInstalledPackages(0);
+
+        for (int i = 0, n = packageInfo.size() - 1; i <= n; --n)
+        {
+            final PackageInfo info = packageInfo.get(n);
+            if (!isExternalCoinPackage(info))
+            {
+                packageInfo.remove(n);
+            }
+        }
+        return packageInfo;
+    }
+
+    private boolean isExternalCoinPackage(final PackageInfo info)
+    {
+        final String[] parts = info.packageName.split("\\.");
+        return parts[parts.length - 1].startsWith("coin") && parts[parts.length - 1].endsWith("ext");
+    }
+
+    public String findExternalResourcePackage(final String coinPrefix)
+    {
+        final List<PackageInfo> pkgs = findExternalPackages();
+        for (final PackageInfo pkg : pkgs)
+        {
+            try
+            {
+                final Resources res = mContext.getPackageManager().getResourcesForApplication(pkg.packageName);
+
+                if (getExternalResourceEdge(pkg.packageName, res, coinPrefix) != 0 ||
+                    getExternalResourceHeads(pkg.packageName, res, coinPrefix) != 0 ||
+                    getExternalResourceEdge(pkg.packageName, res, coinPrefix) != 0)
+                {
+                    return pkg.packageName;
+                }
+            }
+            catch (final NameNotFoundException e)
+            {
+                // The resources probably wasn't in there anyway
+            }
+        }
+        return null;
+    }
+
+    public int getExternalResourceHeads(final String packageName, final Resources pkg, final String prefix)
+    {
+        return pkg.getIdentifier(prefix + "_heads", "drawable", packageName);
+    }
+
+    public int getExternalResourceTails(final String packageName, final Resources pkg, final String prefix)
+    {
+        return pkg.getIdentifier(prefix + "_tails", "drawable", packageName);
+    }
+
+    public int getExternalResourceEdge(final String packageName, final Resources pkg, final String prefix)
+    {
+        return pkg.getIdentifier(prefix + "_edge", "drawable", packageName);
     }
 
 }
